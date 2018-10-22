baa9187e48f1e31b03c19a0fc9e5a1d2ec09f978
ThanosAlmighty <32068435+ThanosAlmighty@users.noreply.github.com>
2018-10-22 02:44:35 -0400

    changed js approach

--- website/js/current_page.js
+++ website/js/current_page.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   var url = window.location.pathname;
   var filename = url.substring(url.lastIndexOf('/')+1);
-alert(filename);
-    $("#" + filename).attr("class","current");
+  $("#" + filename).css({"background-color": "#e14658", "border": "2px solid #FFFFFF","color": "#22252C";});
 });
