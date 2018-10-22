1a6c29f2d492801a73e13d60dc78797cbc16b36d
ThanosAlmighty <32068435+ThanosAlmighty@users.noreply.github.com>
2018-10-22 02:45:20 -0400

    added script tags to index

--- website/index.php
+++ website/index.php
@@ -21,6 +21,8 @@ $errors = array();
 	<meta name="author" content="Jonathan Huling">
 	<link rel="stylesheet" href="css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="js/current_page.js"></script>
 </head>
 <body>
 	<?php include 'include/header.php'; ?>
