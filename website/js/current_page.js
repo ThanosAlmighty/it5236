6544d05f9a9bf9ecab9fa8e481fa63ffe56fd117
ThanosAlmighty <32068435+ThanosAlmighty@users.noreply.github.com>
2018-10-22 02:46:58 -0400

    syntax error fixed

--- website/js/current_page.js
+++ website/js/current_page.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   var url = window.location.pathname;
   var filename = url.substring(url.lastIndexOf('/')+1);
-  $("#" + filename).css({"background-color": "#e14658", "border": "2px solid #FFFFFF","color": "#22252C";});
+  $("#" + filename).css({"background-color": "#e14658", "border": "2px solid #FFFFFF","color": "#22252C"});
 });
