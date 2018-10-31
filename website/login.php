637e029ee4925054d6bf9da4e5afae3cc1acfb64
ThanosAlmighty <32068435+ThanosAlmighty@users.noreply.github.com>
2018-10-31 14:23:16 -0400

    debug tweaks

--- website/login.php
+++ website/login.php
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 	// Attempt to login the user and capture the result flag
 	$result = $app->login($username, $password, $errors);
-	echo $result;
+	print_r($result);
 	// Check to see if the login attempt succeeded
 	if ($result != FALSE) {
 		if($app->create_otp($result['email'], $result['sessionid'], $errors)){
