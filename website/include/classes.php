c8994ca1ae733035606d654eadbb16e8835327ea
ThanosAlmighty <32068435+ThanosAlmighty@users.noreply.github.com>
2018-11-07 22:36:25 -0500

    debugging

--- website/include/classes.php
+++ website/include/classes.php
@@ -379,7 +379,7 @@ class Application {
        				} else if($httpCode == 200) {
                 // If the query did not run successfully, add an error message to the list
                 if ($result == 0) {
-                    $errors[] = "An unexpected error occurred";
+                    $errors[] = "An unexpected error occurred creating a session";
                     $this->debug("Server failed to insert session");
                     $this->auditlog("new session error", "Server failed to insert session");
                 } else if($result == 1) {
